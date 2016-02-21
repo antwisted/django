18bbb82a1b4cea8aae7dc16b74f3d3d91ce38424
Marina Rozova <marinar578@gmail.com>
2016-02-21 16:47:01 -0500

    configure routes and views, create models and migrations

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'feedback.apps.FeedbackConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
